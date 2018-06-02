4ffca016f3e8c9e8c18e736d09e14fea374b129f
Dani Lupión <danilupion@gmail.com>
2018-06-03 00:51:25 +0200

    Like and unlike functional

--- src/routes/api/gifs/likes.js
+++ src/routes/api/gifs/likes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const { Types } = require('mongoose');
 
-const { InternalServerError } = require('../../../error/httpStatusCodeErrors');
+const { Gif } = require('../../../models');
+const { InternalServerError, BadRequest } = require('../../../error/httpStatusCodeErrors');
 const jwtAuthMiddleware = require('../../../middlewares/express/jwt-auth');
 
 const router = new express.Router();
@@ -10,25 +11,55 @@ const router = new express.Router();
  * Route: /api/gifs/:id/likes
  * Method: POST
  *
- * Adds a new like to a given gif
+ * Adds a new like to a given gif if token user did not already like it
  */
 router.post('/', jwtAuthMiddleware, async (req, res) => {
   try {
-    await req.gif
-      .populate({
-        path: 'likes.user',
-        select: 'username _id',
-        match: { _id: req.user.id },
-      })
-      .execPopulate();
+    // Check if the gif is already liked by current user
+    const gif = await Gif.findOne({
+      // eslint-disable-next-line no-underscore-dangle
+      _id: req.gif._id,
+      likes: { $elemMatch: { user: Types.ObjectId(req.user.id) } },
+    });
 
-    // req.gif.likes = req.gif.likes.filter(l => l.user !== null);
+    if (gif) {
+      return res.errorHandler(new BadRequest());
+    }
 
-    console.log(req.gif.likes, req.user, typeof Types.ObjectId(req.user.id));
     req.gif.likes.push({
       user: Types.ObjectId(req.user.id),
     });
 
+    await req.gif.save();
+
+    return res.send();
+  } catch (err) {
+    return res.errorHandler(new InternalServerError(err));
+  }
+});
+
+/**
+ * Route: /api/gifs/:id/likes
+ * Method: DELETE
+ *
+ * Removes a like from a gif if token user liked it
+ */
+router.delete('/', jwtAuthMiddleware, async (req, res) => {
+  try {
+    // Check if the gif is already liked by current user
+    const gif = await Gif.findOne({
+      // eslint-disable-next-line no-underscore-dangle
+      _id: req.gif._id,
+      likes: { $elemMatch: { user: Types.ObjectId(req.user.id) } },
+    });
+
+    if (!gif) {
+      return res.errorHandler(new BadRequest());
+    }
+
+    const like = gif.likes.find(l => l.user.equals(req.user.id));
+    // eslint-disable-next-line no-underscore-dangle
+    req.gif.likes.id(like._id).remove();
     await req.gif.save();
 
     return res.send();
